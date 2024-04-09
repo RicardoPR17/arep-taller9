918a3895568095bb5f21b5718b7650a7fe3fa262
ricardo.pulido <rpulidor17.rpr18@gmail.com>
2024-04-09 17:38:53 -0500

    Archivos base - Parte 3 error

---
+++ RAGPinecone.py
@@ -0,0 +1,68 @@
+from langchain_community.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import Pinecone
+from langchain_pinecone import PineconeVectorStore
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["PINECONE_API_KEY"] = "4151c3cb-1222-4f3c-84bc-064dcaa77838"
+os.environ["PINECONE_ENV"] = "gcp-starter"
+
+
+def load_text():
+    loader = TextLoader("./paper.txt")
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+
+    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len, is_separator_regex=False,)
+
+    docs = text_splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings()
+
+    import pinecone
+
+    # initialize pinecone
+    pinecone.init(
+        api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
+        environment=os.getenv("PINECONE_ENV"),  # next to api key in console
+    )
+
+    index_name = "taller9"
+
+    docsearch = PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
+
+    # First, check if our index already exists. If it doesn't, we create it
+    if index_name not in pinecone.list_indexes():
+        # we create a new index
+        pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
+    # The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
+    # docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+
+    query = "Como funciona Google Flu Trends"
+    docs = docsearch.similarity_search(query)
+    print(docs[0].page_content)
+
+
+def search():
+    embeddings = OpenAIEmbeddings()
+    import pinecone
+
+    # initialize pinecone
+    pinecone.init(
+        api_key=os.getenv("PINECONE_API_KEY"),  # find at app.pinecone.io
+        environment=os.getenv("PINECONE_ENV"),  # next to api key in console
+    )
+
+    index_name = "langchain-demo"
+    # if you already have an index, you can load it like this
+    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+    query = "What is Matrix"
+    docs = docsearch.similarity_search(query)
+
+    print(docs[0].page_content)
+
+
+load_text()
